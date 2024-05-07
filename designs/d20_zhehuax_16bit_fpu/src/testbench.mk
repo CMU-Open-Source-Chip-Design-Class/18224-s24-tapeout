48e75ef2ca52a5510a6d29cb9000780c88128e86
Petros-Emmanouilidis <113743927+Petros-Emmanouilidis@users.noreply.github.com>
2024-05-07 21:51:50 +0300

    Delete designs/d20_zhehuax_16bit_fpu/src directory
    Start over!

--- designs/d20_zhehuax_16bit_fpu/src/testbench.mk
+++
@@ -1,7 +0,0 @@
-TOPLEVEL_LANG = verilog
-VERILOG_SOURCES = $(shell pwd)/top.sv input_16.sv output_16.sv fpu_16bit.sv
-TOPLEVEL = top
-MODULE = testbench
-SIM = verilator
-EXTRA_ARGS += --trace --trace-structs -Wno-fatal
-include $(shell cocotb-config --makefiles)/Makefile.sim
